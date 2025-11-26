cb5b6fc508524ed403b7e4b870bf8f9066cbf78f
Mupang <eorhkd7078@naver.com>
2025-11-26 17:51:49 +0900

    강대광 - 모집글 html/css

--- BC/recruitment/urls.py
+++ BC/recruitment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='list')
+    path('', views.list, name='list'),
+    path('write/', views.write, name = 'write')
 ]
--- BC/recruitment/views.py
+++ BC/recruitment/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def list(request):
     return render(request, 'list.html')
+
+def write(request):
+    return render(request, 'write.html')
