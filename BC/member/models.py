fe5ed5b796446022449f4e9df1bbb525ef18630d
muzzing <kingzchoi@gmail.com>
2025-11-27 12:32:35 +0900

    # 4-2 최무선 faq페이지 및 자유게시판 페이지 초안 + ERD수정으로 인한 models 일부 수정

--- BC/member/models.py
+++ BC/member/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # -----------------------------------------------------
 class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
     user_id = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20, unique=True)
