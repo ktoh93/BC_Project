ea5b8d29691744d7b1a77a0f59bc05d6ac0c0018
Mupang <eorhkd7078@naver.com>
2025-11-27 10:50:57 +0900

    강대광 - 모집할래 셀렉터 수정

--- BC/manager/views.py
+++ BC/manager/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
 def manager(request):
+    if request.method == "POST":
+        admin_id = request.POST.get('admin_id')
+        admin_pw = request.POST.get('admin_pw')
+
+        # TODO : db 연결해서 비교 필요 
+        """
+        admin = db(username=admin_id, password=admin_pw)
+
+        if admin is not None:
+            login(request, admin)
+            return redirect(/manager/dashboard/')
+        else:
+           return render(request, "manager/", {
+                "error": "관리자의 아이디 혹은 비밀번호가 아닙니다."
+            })
+        """
     return render(request, 'login_manager.html')
